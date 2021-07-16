efeef6432951a4e9c3716ec0844aea68afa7f48f
christianxdev <chrismbuthiaofficial@gmail.com>
2021-07-16 10:06:06 +0300

    meetups

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,30 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://meetups-1db3e-default-rtdb.firebaseio.com/Meetups Data.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
